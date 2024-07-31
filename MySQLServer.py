2917f6088ca2543469a2d10ee6a7e1516032bfb3
unknown <mcclaude7>
2024-07-31 12:49:43 +0200

    Updated mysqlserver.py file

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,45 +1,33 @@
-
 import mysql.connector
-from mysql.connector import errorcode
-
-def create_database(cursor, db_name):
+from mysql.connector import Error
+def create_database():
     try:
-        cursor.execute(f"CREATE DATABASE IF NOT EXISTS {db_name}")
-        print(f"Database '{db_name}' created successfully!")
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_DB_CREATE_EXISTS:
-            print(f"Database '{db_name}' already exists.")
-        else:
-            print(f"Failed creating database: {err}")
+        mydb = mysql.connector.connect(
+        host = 'localhost',
+        user = 'root',
+        password = 'Kabe@9168Clde'
+        )
+        if mydb.is_connected():
+          
+          mycursor = mydb.cursor()
+          try:
+            mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+            mydb.commit()
+            print("Database 'alx_book_store' created successfully!")
 
-def main():
-    # Define your MySQL connection parameters
-    config = {
-        'user': 'root',
-        'password': 'Kabe@9168Clde',
-        'host': 'localhost'
-    }
 
-    try:
-        # Establish a connection to MySQL server
-        cnx = mysql.connector.connect(**config)
-        cursor = cnx.cursor()
-
-        # Database name
-        db_name = 'alx_book_store'
-
-        # Create the database
-        create_database(cursor, db_name)
-
-        # Close the cursor and connection
-        cursor.close()
-        cnx.close()
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
-    else:
-        print("Connection to MySQL server closed.")
-
-if __name__ == '__main__':
-    main()
-
+          except Error as e:
+            if "1007 (HY000): Can't create database" in str(e):
+                print("Database 'alx_book_store' already exists!")
+            else:
+                print(f'error creating database {e}')
+          finally:
+        
+            mycursor.close()
+    except Error as e:
+       print(f"Error connecting to MySQL: {e}")
+    finally:
+        if mydb.is_connected():
+           mydb.close()
+        print('Mysql connection is closed') 
 
